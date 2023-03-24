dbc8e3aa0d25ee07ec2be4c38cbc3b53f04a40de
Shingo Morimoto <shingo@wolfssl.com>
2023-03-24 15:22:00 +0900

    Add the implementation of the method SendSlack() to notify to slack channel

--- UpdateNotify.py
+++ UpdateNotify.py
@@ -1,5 +1,7 @@
 # pip install PyGithub if not already installed PyGithub.
 from github import Github
+# !pip install slack_sdk
+from slack_sdk import WebClient
 import datetime
 import sys
 import os
@@ -7,36 +9,59 @@ import smtplib
 from email.mime.multipart import  MIMEMultipart
 from email.mime.text import MIMEText
 
-import last_commit
+import wolfssl_last_commit
+import documentation_last_commit
+
 #================== Configrations ==========================#
-ACCESS_TOKEN="<GitHubのアクセストークン> "
+ACCESS_TOKEN="<Github のアクセストークン> "
 
 mailing_list = [
-    "kojo@wolfssl.com",
+    # "kojo@wolfssl.com",
     "shingo@wolfssl.com"
     # "hide@wolfssl.com",
     # "tak@wolfssl.com"
 ]
 
-MAIL_ADDRESS = "<Gmail のアドレス> "
-MAIL_PASSWORD = "<Gmail のアプリケーションパスワード>" # ログインパスワードではない
+MAIL_ADDRESS = "送信元Gメールアドレス"
+MAIL_PASSWORD = "アプリケーショントークンのパスワード" # ログインパスワードではない
+
+TEST=False
+
+if TEST==True:
+    SLACK_APP_TOKEN = ''
+    CHANNEL_ID = 'C04GL09TA7L' # channel ID of general in My Private workSpace
+
+else:
+    SLACK_APP_TOKEN = ''
+    CHANNEL_ID = 'C029YM5K1J7' # channel ID of japan in wolfSSL workSpace 
 #===========================================================#
 
 
-# Set up 
-def Setup():
-    print("Called Setup!!")
+def writeLog(file, str):
+    with open(file, mode="a") as f:
+        print(str, file=f)
 
 
-def main():
+def watchDoc_md():
     g = Github(ACCESS_TOKEN)
     repo = g.get_repo("wolfssl/documentation")
 
     # Check whether the LAST_COMMIT_SHA is None or not.
     try:
-        Last_commit_sha = last_commit.LAST_COMMIT_SHA
-    except:
-        Setup()
+        Last_commit_sha = documentation_last_commit.LAST_COMMIT_SHA
+
+    except AttributeError:
+    # Initialize If the LAST_COMMIT_SHA is not found
+        # Get the latest commit
+        Last_commit = repo.get_commit("master")
+        print(Last_commit.commit.committer.date)
+
+        # Set the Hash of latest Commit to LAST_COMMIT_SHA in documentation_last_commit.py
+        with open('documentation_last_commit.py', mode = "w") as f:
+            f.write(f'LAST_COMMIT_SHA = \"{Last_commit.commit.sha}\"')
+        print(f"Initialized {Last_commit.commit.sha[:7]}..")
+        writeLog('documentation.log', f"[~] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Initialized {Last_commit.commit.sha[:7]}..")
+
         sys.exit(1)
 
 
@@ -53,10 +78,11 @@ def main():
     # Check if the last commit is not out of date.
     if new_commits.totalCount == 0:
         print("Not Out of Date!")
+        writeLog('documentation.log', f"[*] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Not Out of Date {Last_commit.commit.sha[:7]}..")
         sys.exit(2)
 
 
-    # Check Whether the Contents is updated or not.
+# Check Whether the Contents is updated or not.
     Notification_list = []
 
     for commit in new_commits:
@@ -77,9 +103,11 @@ def main():
         
 
     
-    # If there is not Updated Contents
+    # If there is no Updated Contents
     if len(Notification_list) == 0:
-        print("Newly Merged but Not updated Contents")
+        print("Newly Merged commit but No updated Contents")
+        writeLog('documentation.log', f"[*] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Contents Not Updated!  {new_commits[0].commit.sha[:7]}..")
+
         
  
         
@@ -90,24 +118,122 @@ def main():
         # Do Notification
         for commit in reversed(Notification_list):
             print(commit.commit.committer.date)
-            notifier = Notifier(commit,)
+            notifier = Notifier(commit, subject="Documentation Updated !")
             notifier.gen_msg()
 
             for to_addr in mailing_list:
 
-                notifier.Sendmail(to_addr)
+                # notifier.Sendmail(to_addr)
+                notifier.SendSlack()
+            writeLog('documentation.log', f"[+] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Notified Updated Contents {commit.commit.sha[:7]}..")
 
 
-    # Set the Newest Commit to LAST_COMMIT_SHA in last_commit.py
-    with open('last_commit.py', mode = "w") as f:
+
+    # Set the Newest Commit to LAST_COMMIT_SHA in documentation_last_commit.py
+    with open('documentation_last_commit.py', mode = "w") as f:
         f.write(f'LAST_COMMIT_SHA = \"{new_commits[0].commit.sha}\"')
 
 
 
+
+
+
+def watchDoc_header():
+    g = Github(ACCESS_TOKEN)
+    repo = g.get_repo("wolfssl/wolfssl")
+
+    # Check whether the LAST_COMMIT_SHA is None or not.
+    try:
+        Last_commit_sha = wolfssl_last_commit.LAST_COMMIT_SHA
+    except AttributeError:
+    # Initialize If the LAST_COMMIT_SHA is not found
+        # Get the latest commit
+        Last_commit = repo.get_commit("master")
+        print(Last_commit.commit.committer.date)
+
+        # Set the Hash of latest Commit to LAST_COMMIT_SHA in wolfssl_last_commit.py
+        with open('wolfssl_last_commit.py', mode = "w") as f:
+            f.write(f'LAST_COMMIT_SHA = \"{Last_commit.commit.sha}\"')
+        print(f"Initialized {Last_commit.commit.sha[:7]}..")
+        writeLog('wolfssl_repo.log', f"[~] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Initialized {Last_commit.commit.sha[:7]}..")
+
+        sys.exit(1)
+
+
+    # Get the object of Last commit
+    Last_commit = repo.get_commit(Last_commit_sha)
+
+    Last_updated_datetime = Last_commit.commit.committer.date
+
+    # Get the newly updated commits.
+    # path を指定すると https://github.com/wolfSSL/wolfssl/commits/master/{path} のコミットだけを取得できる
+    # ロードするコミット数を減らせるが、仕組みが少し変わってしまうため　Merge されたコミットだけを見るというロジックが使えない。
+    new_commits = repo.get_commits(since=(Last_updated_datetime+datetime.timedelta(minutes=1)) )
+
+    print("Num of New commits", new_commits.totalCount)
+
+    # # Check if the last commit is not out of date.
+    if new_commits.totalCount == 0:
+        print("Not Out of Date!")
+        writeLog('wolfssl_repo.log', f"[*] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Not Out of Date {Last_commit.commit.sha[:7]}..")
+        sys.exit(2)
+
+
+    # Check Whether the Contents is updated or not.
+    Notification_list = []
+
+    for commit in new_commits:
+        # Marge された時のコミットだけを見る。 （committerのcommitと重複してるから）
+        if "Merge pull request" in commit.commit.message:
+
+            Notify=False 
+            for file in commit.files:
+                # print(file.filename)
+                if "doc/dox_comments" in file.filename \
+                            and "header_files-ja" not in file.filename:
+                    Notify=True
+                
+            if Notify==True:
+                Notification_list.append(commit)
+                # print(file.filename, commit.commit.author.name, commit.commit.committer.date, commit.commit.html_url)
+        
+
+    
+    # If there is no Updated Contents
+    if len(Notification_list) == 0:
+        print("Newly Merged commit but No updated Contents")
+        writeLog('wolfssl_repo.log', f"[*] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Contents Not Updated!  {new_commits[0].commit.sha[:7]}..")
+
+        
+    else:   # Updated Contents Exist!
+        print("Updated Contents Exist!")
+        print("Num of Updated Contents: ", len(Notification_list))
+        
+        # Do Notification
+        for commit in reversed(Notification_list):
+            print(commit.commit.committer.date)
+            notifier = Notifier(commit, subject="Documentation Updated !")
+            notifier.gen_msg()
+
+            for to_addr in mailing_list:
+
+                # notifier.Sendmail(to_addr)
+                notifier.SendSlack()
+            writeLog('wolfssl_repo.log', f"[+] {datetime.datetime.today().strftime('%Y/%m/%d %H:%M')} : Notified Updated Contents {commit.commit.sha[:7]}..")
+
+
+
+    # Set the Newest Commit to LAST_COMMIT_SHA in wolfssl_last_commit.py
+    with open('wolfssl_last_commit.py', mode = "w") as f:
+        f.write(f'LAST_COMMIT_SHA = \"{new_commits[0].commit.sha}\"')
+    
+
+
 class Notifier():
 
-    def __init__(self, commit, msg=""):
+    def __init__(self, commit, subject="", msg=""):
         self.commit = commit
+        self.subject=subject
         self.msg = msg
 
         self.updated_files = []
@@ -124,9 +250,9 @@ class Notifier():
         msg += f"[Date]         {self.commit.commit.committer.date}\n"
         msg += f"[Merged by]    {self.commit.commit.author.name}\n"
         msg += f"[Commit Message]\n"
-        msg += f"{self.commit.commit.message}\n"
+        msg += f"{self.commit.commit.message}\n\n"
         
-        msg += f"[Changed Files]\n\n"
+        msg += f"[Changed Files]\n"
         for filename in self.updated_files:
             msg += f"{filename}\n"
         msg += "\n"
@@ -136,7 +262,7 @@ class Notifier():
         self.msg = msg
 
 
-    def Sendmail(self, to_addr="shingo@wolfssl.com"):
+    def Sendmail(self, to_addr=""):
         smtp_server = "smtp.gmail.com"
         port = 587
 
@@ -147,7 +273,7 @@ class Notifier():
 
         message = MIMEMultipart()
 
-        message["Subject"] = "Documentation Updated!"
+        message["Subject"] = self.subject
         message["From"] = MAIL_ADDRESS
         message["To"] = to_addr
 
@@ -159,11 +285,27 @@ class Notifier():
 
         server.quit()
 
-    def SendLINE():
+    def SendLINE(self):
         pass
 
-    def SendSlack():
-        pass
+    def SendSlack(self):
+        # Create the Slack Client
+        client = WebClient(SLACK_APP_TOKEN)
+        text= self.subject + "\n" + self.msg
+        # Send text to the Slack Channel
+        responce = client.chat_postMessage(channel=CHANNEL_ID, text=text)
+
+
+
+def getMerges():
+    g = Github(ACCESS_TOKEN)
+    repo = g.get_repo("wolfssl/wolfssl")
+    print(repo.merges_url)
+
+
+
+
     
 if __name__ == '__main__':
-    main()
+    watchDoc_md()
+    watchDoc_header()
